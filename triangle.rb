b6f12db7de1c41ff87749d13fcaddc7de077bc7d
GreenJoey <esyray15@gmail.com>
2017-12-28 21:08:36 +0530

    Finished the Trinagle Exception problem

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,11 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+    if a <= 0 || b <= 0 || c <= 0
+      raise TriangleError
+    elsif a+b <= c || b+c <= a || a+c <= b
+      raise TriangleError
+    end
     if a == b && b == c
         return :equilateral
     elsif a == b || b == c || c == a
